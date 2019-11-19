52a35d19f92dbc6b8bc2f951e8de0cc525410702
ggam646313 <wjstmdgusz@naver.com>
2019-11-19 17:40:13 +0900

    11 19

---
+++ app/src/main/java/com/example/a6wproject3/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.a6wproject3;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private FragmentHome fragmentHome= new FragmentHome();
+    private FragmentSearch fragmentSearch = new FragmentSearch();
+    private FragmentUpload fragmentUpload = new FragmentUpload();
+    private FragmentFavorite fragmentFavorite = new FragmentFavorite();
+    private FragmentMy fragmentMy = new FragmentMy();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss();
+
+        BottomNavigationView bnv = findViewById(R.id.bnv);
+        bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+                switch(menuItem.getItemId())
+                {
+                    case R.id.bnv_home:
+                        transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss();
+                        break;
+
+                    case R.id.bnv_search:
+                        transaction.replace(R.id.frameLayout, fragmentSearch).commitAllowingStateLoss();
+                        break;
+
+                    case R.id.bnv_upload:
+                        transaction.replace(R.id.frameLayout, fragmentUpload).commitAllowingStateLoss();
+                        break;
+
+                    case R.id.bnv_favorite:
+                        transaction.replace(R.id.frameLayout, fragmentFavorite).commitAllowingStateLoss();
+                        break;
+
+                    case R.id.bnv_my:
+                        transaction.replace(R.id.frameLayout, fragmentMy).commitAllowingStateLoss();
+                        break;
+                }
+                return true;
+            }
+        });
+
+    }
+}
