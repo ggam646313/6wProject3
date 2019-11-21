7de931baced364f55f7ee51a47bbdcbfa26b5698
ggam646313 <wjstmdgusz@naver.com>
2019-11-21 17:30:21 +0900

    11 21

--- app/src/main/java/com/example/a6wproject3/FragmentMy.java
+++ app/src/main/java/com/example/a6wproject3/FragmentMy.java
@@ -8,12 +8,44 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class FragmentMy extends Fragment {
+
+    TabLayout tabLayout;
+    ViewPager pager;
+    MyAdapter2 adapter2;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_my, container, false);
+        View view= inflater.inflate(R.layout.fragment_my, container, false);
+
+        tabLayout= view.findViewById(R.id.layout_tab);
+        pager= view.findViewById(R.id.pager2);
+        adapter2= new MyAdapter2(getChildFragmentManager());
+        pager.setAdapter(adapter2);
+        tabLayout.setupWithViewPager(pager);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/a6wproject3/MyAdapter2.java
@@ -0,0 +1,38 @@
+package com.example.a6wproject3;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class MyAdapter2 extends FragmentPagerAdapter {
+
+    Fragment[] fragments= new Fragment[2];
+    String[] pageTitles= new String[]{"A", "B"};
+
+    public MyAdapter2(@NonNull FragmentManager fm) {
+        super(fm);
+
+        fragments[0]= new Page1FragmentMy();
+        fragments[1]= new Page2FragmentMy();
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragments[position];
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.length;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        return pageTitles[position];
+    }
+}
